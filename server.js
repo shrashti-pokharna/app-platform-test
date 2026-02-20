3c943a831a250f87ef9c2fa026c10e1bd1c9d885
Shrashti Pokharna <158441181+shrashti-pokharna@users.noreply.github.com>
2026-02-20 18:12:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const PORT = 8080;
 
 http.createServer((req,res)=>{
-res.end("App Platform Working");
+res.end("App Platform Working Now");
 }).listen(PORT, ()=>{
 console.log("Server running on port " + PORT);
 });
